ddf56c3e49404f528b8a30b3b87448cb37ed5c6d
Tara Regan <tararegan93@gmail.com>
2021-04-09 12:16:57 +1000

    added app.use middleware

--- src/app.js
+++ src/app.js
@@ -4,30 +4,33 @@ const app = express();
 app.use(express.json());
 
 app.post('/', function (req, res) {
-    try{
-        if (req.body && req.body.payload) {
-            let filteredShows = []
-            req.body.payload.forEach( (show) => {
-                if (
-                    show.drm && 
-                    typeof(show.drm) === 'boolean' &&
-                    show.drm == true && 
-                    show.episodeCount && 
-                    typeof(show.episodeCount) === 'number' &&
-                    show.episodeCount > 0
-                ) {
-                    filteredShows.push(createShowRes(show))
-                }
-            })
-            res.json({ response: filteredShows });
-        }
-        else { throw "Invalid JSON"}
-    } catch (e) {
-        res.status(400).json({ error: 'Could not decode request: JSON parsing failed' })}
+    if (req.body && req.body.payload) {
+        let filteredShows = []
+        req.body.payload.forEach( (show) => {
+            if (
+                show.drm && 
+                typeof(show.drm) === 'boolean' &&
+                show.drm == true && 
+                show.episodeCount && 
+                typeof(show.episodeCount) === 'number' &&
+                show.episodeCount > 0
+            ) {
+                filteredShows.push(createShowRes(show))
+            }
+        })
+        res.json({ response: filteredShows });
+    }
+    else { throw "Invalid JSON"}
 });
 
 app.all('*', function (req, res) {
+    throw "Bad request"
+})
+
+app.use(function(e, req, res, next) {
+    if (e) {
     res.status(400).json({ error: 'Could not decode request: JSON parsing failed' })
+    }
 });
 
 const createShowRes = (showReq) => {
